9c0f02b988da6c6fd257f0ce767c732c3f0f1f76
Sebastien Cadorette <cado.94@me.com>
2018-10-12 22:14:23 -0400

    first useless test using eclipse.

---
+++ src/main/java/TP2/TP2.java
@@ -0,0 +1,30 @@
+package TP2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.Exception;
+
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.GraphGenerator;
+
+class TP2 {
+
+	Graph graphe;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
+	@Test
+	void test1() {
+		assertEquals(true, true);
+	}
+}
